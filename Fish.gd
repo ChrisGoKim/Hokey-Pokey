f5b88cb1515fff61b090d02139cfc8605b716886
ChrisGoKim <chrisjokim@gmail.com>
2019-01-26 04:24:19 -0500

    Fleshed out the basic character will basic nodes. Created upgrade UI and randomized logic. Brought in first iterations of Jon's sprite and a walking animation.

--- Fish.gd
+++ Fish.gd
@@ -1,24 +1,51 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
-var programming = randi() % 6
-var audio = randi() % 6
-var art = randi() % 6
-var design = randi() % 6
+enum test{PROGRAMMING, AUDIO, ART, DESIGN}
 
+var stats = [
+	randi() % 6, #programming
+	randi() % 6, #audio
+	randi() % 6, #art
+	randi() % 6 #design
+]
+
+var anim = ""
+
+var canUpgrade = false
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	print(programming)
-	print(audio)
+	anim = "Walk"
+	$AnimateFish.play(anim)
+	#$AnimateFish.playing = true	
+	pass
+
+func _fixed_process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
 	
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+##ALL FUNCTIONALITY FOR UPGRADES
+func boostStat(var num):
+	stats[num] += (randi() % 6)
+	print(stats)
+	pass
+
+func _on_Upgrade_Programming_pressed():
+	boostStat(test.PROGRAMMING)
+	pass # replace with function body
+
+func _on_Upgrade_Audio_pressed():
+	boostStat(test.AUDIO)
+	pass # replace with function body
+
+func _on_Upgrade_Art_pressed():
+	boostStat(test.ART)
+	pass # replace with function body
+
+func _on_Upgrade_Design_pressed():
+	boostStat(test.DESIGN)
+	pass # replace with function body
+
+
